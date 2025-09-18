722257553ede25cd45f7c2d29dabdf95806377fe
VIJAYSRINIVAS <vijaysrinivas200410@gmail.com>
2025-09-18 19:17:38 +0530

    Update SoilAnalysis.js

--- frontend/src/pages/SoilAnalysis.js
+++ frontend/src/pages/SoilAnalysis.js
@@ -33,7 +33,7 @@ const SoilAnalysis = () => {
 
     setLoading(true);
     try {
-      const res = await axios.post('http://localhost:5000/api/soil-analysis', formData);
+      const res = await axios.post('https://fertilizer-recommendation-0qbu.onrender.com/api/soil-analysis', formData);
       if (res.data.success) {
         setAnalysis(res.data.analysis);
         toast.success('Soil analysis completed');
